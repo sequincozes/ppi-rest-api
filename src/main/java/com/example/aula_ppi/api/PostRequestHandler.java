121b64e22dce9828e544c28b2c832c15a9fbd718
sequincozes <sequincozes@gmail.com>
2022-07-14 03:17:07 -0300

    First code.

--- src/main/java/com/example/aula_ppi/api/PostRequestHandler.java
+++ src/main/java/com/example/aula_ppi/api/PostRequestHandler.java
@@ -18,85 +18,90 @@ import java.io.PrintWriter;
  */
 public class PostRequestHandler extends RequestHandler {
 
-	
-	public PostRequestHandler(PersonenModel model) {
-		super(model);
-	}
 
-	
-	@Override
-	public void handleRequest(HttpServletRequest request, HttpServletResponse response, String id)
-	throws ServletException, IOException {
-		
-		// get writer and set the content type of the response to be XML
-		PrintWriter responseWriter = response.getWriter();
-		response.setContentType("application/xml");
-		response.setCharacterEncoding("UTF-8");
-		
-		try {
-			if (id == null) {
-				// add a person (single item of the collection)
-				addPerson(request, response);
-			}
-			else {
-				// this is generally not used
-				String error = "This method type is not allowed for the address.";
-				response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-				responseWriter.println(failureMessage(error));
-				return;
-			}
-		}
-		catch (Exception e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			responseWriter.println(failureMessage(e.getMessage()));
-		}
-	}
+    public PostRequestHandler(PersonenModel model) {
+        super(model);
+    }
 
-	
-	/** Add a new person to the XML-Document. */
-	private void addPerson(HttpServletRequest request, HttpServletResponse response)
-	throws Exception {
-		
-		// add the person to the collection
-		String id = addPerson(null, request.getReader());
-		
-		// marshal the XML-Document (save changes to the file)
-		try {
+
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response, String id)
+            throws ServletException, IOException {
+
+        // get writer and set the content type of the response to be XML
+        PrintWriter responseWriter = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        responseWriter.println("{}");
+        return;
+
+//		try {
+//			if (id == null) {
+//				// add a person (single item of the collection)
+//				addPerson(request, response);
+//			}
+//			else {
+//				// this is generally not used
+//				String error = "This method type is not allowed for the address.";
+//				response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+//				responseWriter.println(failureMessage(error));
+//				return;
+//			}
+//		}
+//		catch (Exception e) {
+//			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+//			responseWriter.println(failureMessage(e.getMessage()));
+//		}
+    }
+
+
+    /**
+     * Add a new person to the XML-Document.
+     */
+    private void addPerson(HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+
+        // add the person to the collection
+        String id = addPerson(null, request.getReader());
+
+        // marshal the XML-Document (save changes to the file)
+        try {
 //			getModel().marshal();
-		}
-		catch (Exception e) {
-			throw new Exception("Marshalling failed. (" + e.getMessage() + ")");
-		}
-		
-		// print information about added person
-		System.out.println("Person added (" + id + ").");
-		
-		// redirect and change method to GET
-		//response.setStatus(HttpServletResponse.SC_SEE_OTHER);
-		//response.setHeader("Location", address);
-		
-		// TODO: works for now, maybe change it to a redirect later
-		new GetRequestHandler(getModel()).handleRequest(request, response, id);
-	}
-	
-	
-	/**
-	 * Add a new person to the collection using the passed body data.<br/>
-	 * (Will not write changes to the XML-Document)
-	 * @param id - can be null
-	 * @return The id of the added person
-	 */
-	public String addPerson(String id, BufferedReader data)
-	throws Exception {
-		
-		// check if the data is available (not empty)
-		if (!data.ready()) {
-			throw new IOException("Missing body data!");
-		}
-		
-		// try to unmarshal to a Person object
+        } catch (Exception e) {
+            throw new Exception("Marshalling failed. (" + e.getMessage() + ")");
+        }
+
+        // print information about added person
+        System.out.println("Person added (" + id + ").");
+
+        // redirect and change method to GET
+        //response.setStatus(HttpServletResponse.SC_SEE_OTHER);
+        //response.setHeader("Location", address);
+
+        // TODO: works for now, maybe change it to a redirect later
+        new GetRequestHandler(getModel()).handleRequest(request, response, id);
+    }
+
+
+    /**
+     * Add a new person to the collection using the passed body data.<br/>
+     * (Will not write changes to the XML-Document)
+     *
+     * @param id - can be null
+     * @return The id of the added person
+     */
+    public String addPerson(String id, BufferedReader data)
+            throws Exception {
+
+        // check if the data is available (not empty)
+//		if (!data.ready()) {
+//			throw new IOException("Missing body data!");
+//		}
+
+        // try to unmarshal to a Person object
 //		PersonType newPerson = getModel().unmarshalPerson(new StreamSource(data));
-		
+
 //		// check that IDs match if the id is given
 //		if (id != null && !newPerson.getId().equals(id)) {
 //			throw new Exception("IDs do not match!");
@@ -105,6 +110,6 @@ public class PostRequestHandler extends RequestHandler {
 //		// add person to collection
 //		getModel().addPerson(newPerson);
 //
-		return "idOfPerson";//newPerson.getId();
-	}
+        return "idOfPerson";//newPerson.getId();
+    }
 }
